bd8a1c7e3cec7f79188fe3bd98c4a4ce7545b055
khanghou <khanghou.tan@sgtradextech.com>
2024-06-14 13:44:34 +0800

    48: faster

--- 49.py
+++ 49.py
@@ -1,45 +1,15 @@
-from collections import Counter
+from collections import Counter, defaultdict
 from typing import List
 
 
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        def isAnagram(s1, s2):
-            map1 = Counter(s1)
-            map2 = Counter(s2)
-            return map1 == map2
+        dict = defaultdict(list)
         
-        n = len(strs)
-        
-        if n == 0:
-            return [[""]]
-        
-        ans = []
-        seen = set()
-
-        '''
-        1. Lock the first number
-        2. Nested for loop the rest of the nubmers in the list 
-        3. In each iteration, append a new array. Then check if word1 and word2 is anagram. If so, then add them to the current array
-        
-        '''
-        for i in range(n):
-            word1 = strs[i]
-            # We skip word because it has already been added to an anagram group
-            if word1 in seen:
-                continue
-            seen.add(word1)
-            ans.append([word1])
-
-            for j in range(i+1, n):
-                word2 = strs[j]
-                same = isAnagram(word1, word2)
-                    
-                if same:
-                    seen.add(word2)
-                    ans[-1].append(word2)
-                
-        return ans
+        for word in strs:
+            sortedWord = ''.join(sorted(word))
+            dict[sortedWord].append(word)
+        return [dict[key] for key in dict]
     
 sln = Solution()
 # ans = sln.groupAnagrams(["abc", "bca", "acb"]) # [["bat"],["nat","tan"],["ate","eat","tea"]]
@@ -49,9 +19,11 @@ ans = sln.groupAnagrams(["ddddddddddg","dgggggggggg"]) # [["bat"],["nat","tan"],
 # ans = sln.groupAnagrams(["eat","tea","tan","ate","nat","bat"]) # [["bat"],["nat","tan"],["ate","eat","tea"]]
 print(ans)
 
-def isAnagram(s1, s2):
-    map = Counter(s1)
-    map2 = Counter(s2)
-    print(map, map2, map == map2)
+# def isAnagram(s1, s2):
+#     map = {}
+#     map["a"] = 0
+#     map["b"] = 1
+#     for key in map:
+#         print(key)
         
-isAnagram("abc", "bca")
+# isAnagram("abc", "bca")
