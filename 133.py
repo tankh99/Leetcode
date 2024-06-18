2917f3347e18977ca8828b54d68cea4203308f42
khanghou <khanghou.tan@sgtradextech.com>
2024-06-18 16:33:19 +0800

    133 clone graph bfs (assisted)

---
+++ 133.py
@@ -0,0 +1,31 @@
+from collections import deque
+"""
+# Definition for a Node.
+"""
+
+class Node:
+    def __init__(self, val = 0, neighbors = None):
+        self.val = val
+        self.neighbors = neighbors if neighbors is not None else []
+
+from typing import Optional
+class Solution:
+    def cloneGraph(self, node: Optional['Node']) -> Optional['Node']:
+        if not node:
+            return None
+        map = {}
+        q = deque()
+        q.append(node)
+        
+        newRoot = Node(node.val)
+        map[node.val] = newRoot
+        while q:
+            curr = q.popleft()
+            for nbr in curr.neighbors:
+                if nbr.val not in map:
+                    map[nbr.val] = Node(nbr.val)
+                    q.append(nbr)
+                map[curr.val].neighbors.append(map[nbr.val])
+        return newRoot
+
+        
