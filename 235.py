d28b1063547ba6065355ff04e8f954c4281959d6
tankh99 <khanghou99@gmail.com>
2024-06-26 05:51:08 +0800

    235.py

---
+++ 235.py
@@ -0,0 +1,23 @@
+
+class Solution:
+    def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
+        if root is None:
+            return None
+        lca = root
+        l, r = root, root
+        while l.val != p.val or r.val != q.val:
+            '''
+            1. p < lca and q > lca (or vice versa), then return root directly
+            2. p < lca and q < lca, then shift lca to left
+            3.. p > lca nad q > lca, then shift lca to left
+            '''
+            isDivergent = (p.val <= lca.val and q.val >= lca.val) or (p.val >= lca.val and q.val <= lca.val)
+            if isDivergent:
+                # print(p.val, q.val, lca.val)
+                return lca
+            
+            elif p.val < lca.val and q.val < lca.val:
+                lca = lca.left
+            else:
+                lca = lca.right
+        
