476a8384ebbe2c8a2ad6ed44aaf485aa24631ab2
tankh99 <khanghou99@gmail.com>
2024-08-13 08:10:50 +0800

    rijeci: mem limit exceeded

--- rijeci.py
+++ rijeci.py
@@ -1,24 +1,31 @@
 from sys import stdin, stdout
 
+map = {}
+
+def build_str(k):
+    if k == 0:
+        return "A"
+    if k == 1:
+        return "B"
+    elif k in map:
+        return map[k]
+    val = build_str(k-1) + build_str(k-2)
+    map[k] = val
+    return map[k]
+
 def rijeci(k):
     string = "A"
-    for _ in range(k):
-        a_count, b_count = 0, 0
-        arr = []
-        for _, c in enumerate(string):
-            if c == "A":
-                arr.append("B")
-                b_count += 1
-            elif c == "B":
-                arr.append("BA")
-                b_count += 1
-                a_count += 1
-
-        string = "".join(arr)
+    string = build_str(k)
+    a_count, b_count = 0, 0
+    for c in string:
+        if c == "A":
+            a_count += 1
+        elif c == "B":
+            b_count += 1
     return a_count, b_count
 
 k = int(stdin.readline().strip())
 
-a,b = rijeci(k)
+a, b = rijeci(k)
 
 stdout.write(f"{a} {b}\n")
